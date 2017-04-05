8f4555f6e12a57ad1748b1649c83db92b70036df
Alejandro Arévalo Romero <alejandro19193@gmail.com>
2017-04-06 00:07:40 +0200

    Creada la entidad usuario la cual nos permite almacenar usuarios asi como sus distintos roles

---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,286 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity
+ */
+class Usuario implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $usuario;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $clave;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $apellidos;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    private $editor;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    private $administrador;
+
+    /**
+     * Convierte el profesor en una cadena de texto
+     */
+    public function __toString()
+    {
+        return $this->getApellidos() . ', ' . $this->getNombre();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellidos
+     *
+     * @param string $apellidos
+     *
+     * @return Usuario
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get apellidos
+     *
+     * @return string
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * @param string $usuario
+     * @return Usuario
+     */
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+        return $this;
+    }
+
+    /**
+     * Set clave
+     *
+     * @param string $clave
+     *
+     * @return Usuario
+     */
+    public function setClave($clave)
+    {
+        $this->clave = $clave;
+
+        return $this;
+    }
+
+    /**
+     * Get clave
+     *
+     * @return string
+     */
+    public function getClave()
+    {
+        return $this->clave;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEditor()
+    {
+        return $this->editor;
+    }
+
+    /**
+     * @param bool $editor
+     * @return Usuario
+     */
+    public function setEditor($editor)
+    {
+        $this->editor = $editor;
+        return $this;
+    }
+
+
+
+    /**
+     * Set administrador
+     *
+     * @param boolean $administrador
+     *
+     * @return Usuario
+     */
+    public function setAdministrador($administrador)
+    {
+        $this->administrador = $administrador;
+
+        return $this;
+    }
+
+    /**
+     * Get administrador
+     *
+     * @return boolean
+     */
+    public function isAdministrador()
+    {
+        return $this->administrador;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        $roles = ['ROLE_USER'];
+
+        if ($this->isAdministrador()) {
+            $roles[] = 'ROLE_ADMIN';
+        }
+
+        if ($this->isEditor()) {
+            $roles[] = 'ROLE_EDITOR';
+        }
+
+        return $roles;
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return $this->getClave();
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->getUsuario();
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+    }
+}

80de0edc08bbacc6d86db82bfb9158140d378137
Alejandro Arévalo Romero <alejandro19193@gmail.com>
2017-04-20 01:21:43 +0200

    Añadido el formulario de la entidad Categoria

---
+++ src/AppBundle/Form/Type/CategoriaType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use AppBundle\Entity\Categoria;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoriaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('numero')
+            ->add('nombre')
+            ->add('padre');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Categoria::class
+        ]);
+    }
+}

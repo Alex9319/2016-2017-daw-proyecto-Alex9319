b5e490449abaf9db3396e0b074a4b42385ee2081
Alejandro Arévalo Romero <alejandro19193@gmail.com>
2017-04-19 20:05:05 +0200

    Modificado el controlador por defecto para hacer la pagina de inicio y el login de usuario y añadida la plantilla de login para la aplicacion

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,11 +12,28 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function inicioAction(Request $request)
     {
+
+        return $this->render('layout.html.twig');
+    }
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction()
+    {
+        $helper = $this->get('security.authentication_utils');
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        return $this->render('aplicacion/login.html.twig', [
+            'error' => $helper->getLastAuthenticationError()
         ]);
+    }
+
+    /**
+     * @Route("/comprobar", name="comprobar")
+     * @Route("/salir", name="salir")
+     */
+    public function comprobarAction() {
     }
 }

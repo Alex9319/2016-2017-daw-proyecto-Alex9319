ed4b8c4a0e01bf74b709c882cd83d877c8be9af5
Alejandro Arévalo Romero <alejandro19193@gmail.com>
2017-04-19 20:21:37 +0200

    Anadida la inserción, modificación y el listado de las categorias

---
+++ src/AppBundle/Controller/CategoriaController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Categoria;
+use AppBundle\Form\Type\CategoriaType;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoriaController extends Controller
+{
+
+    /**
+     * @Route("/categorias", name="listadoCategorias")
+     */
+    public function indexAction()
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $categorias = $em->createQueryBuilder()
+            ->select('c')
+            ->from('AppBundle:Categoria', 'c')
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('categorias/listar.html.twig', [
+            'categorias' => $categorias,
+        ]);
+    }
+
+    /**
+     * @Route("/categoria/modificar/{id}", name="modificar_cat")
+     * @Route("/categoria/nueva", name="nueva_cat")
+     */
+    public function formularioAction(Request $request, Categoria $categoria = null)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        if (null == $categoria) {
+            $categoria = new Categoria();
+            $em->persist($categoria);
+        }
+
+        $form = $this->createForm(CategoriaType::class, $categoria);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('estado', 'Cambios guardados con éxito');
+            return $this->redirectToRoute('listadoCategorias',['categoria'=>$categoria->getId()]);
+        }
+
+        return $this->render('categorias/form.html.twig', [
+            'categoria' => $categoria,
+            'form' => $form->createView()
+        ]);
+    }
+}

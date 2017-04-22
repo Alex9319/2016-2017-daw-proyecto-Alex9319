de4c0cc2aea10ea8f3f2d03fa645ab5d48487f84
Alejandro Arévalo Romero <alejandro19193@gmail.com>
2017-04-22 11:34:25 +0200

    Añadido el efecto del cerrar sesion y el elemento select2 para los select para buscar en ellos

---
+++ web/jquery/index.js
@@ -0,0 +1,9 @@
+$("#usuario").mouseenter(function() {
+	$("#desplegable").show('slow');
+});
+								
+$("#desplegable").mouseleave(function() {
+	$("#desplegable").hide('slow');
+});
+	
+$('select').select2();

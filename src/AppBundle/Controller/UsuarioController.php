60a491b516c381fbc1408669422644b72c769a10
Alejandro Arévalo Romero <alejandro19193@gmail.com>
2017-04-22 14:01:32 +0200

    Editado para que el perfil del usuario pueda ser visible sin tener ningun rol y editado ya que no funcionaba desde el administrador la modificación de otros perfiles

---
+++ src/AppBundle/Controller/UsuarioController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Usuario;
+use AppBundle\Form\Type\UsuarioType;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UsuarioController extends Controller
+{
+
+    /**
+     * @Security("is_granted('ROLE_ADMIN')")
+     * @Route("/usuarios/modificar/{id}", name="musuarios")
+     */
+    public function cambiarUsuarioAction(Request $request, Usuario $usuario) {
+        return $this->cambiarPerfilAction($request, $usuario);
+    }
+
+    /**
+     * @Route("/perfil", name="perfil")
+     */
+    public function cambiarPerfilAction(Request $request, Usuario $usuario = null) {
+        if (null === $usuario) {
+            $usuario = $this->getUser();
+        }
+
+        $form = $this->createForm(UsuarioType::class, $usuario, [
+            'es_admin' => $this->isGranted('ROLE_ADMIN')
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $claveFormulario = $form->get('nueva')->get('first')->getData();
+
+            if ($claveFormulario) {
+                $clave = $this->get('security.password_encoder')
+                    ->encodePassword($usuario, $claveFormulario);
+
+                $usuario->setClave($clave);
+            }
+
+            $this->getDoctrine()->getManager()->flush();
+        }
+        return $this->render('usuarios/form.html.twig', [
+            'usuario'=>$usuario,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/usuarios", name="listadoUsuarios")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function usuariosAction()
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $usuarios = $em->createQueryBuilder()
+            ->select('c')
+            ->from('AppBundle:Usuario', 'c')
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('usuarios/listar.html.twig', [
+            'usuarios' => $usuarios,
+        ]);
+    }
+
+    /**
+     * @Route("/usuarios/nuevo", name="nusuario")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function formularioAction(Request $request, Usuario $usuario = null)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        if (null == $usuario) {
+            $usuario = new Usuario();
+            $em->persist($usuario);
+        }
+
+        $form = $this->createForm(UsuarioType::class, $usuario);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('estado', 'Cambios guardados con éxito');
+            return $this->redirectToRoute('listadoUsuarios',['usuario'=>$usuario->getId()]);
+        }
+
+        return $this->render('usuarios/form.html.twig', [
+            'usuario' => $usuario,
+            'form' => $form->createView()
+        ]);
+    }
+}

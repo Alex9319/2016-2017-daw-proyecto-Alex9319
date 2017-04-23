09a441d3df43b506f73ca2040cb54b581c30f045
Alejandro Arévalo Romero <alejandro19193@gmail.com>
2017-04-23 11:33:57 +0200

    Añadido el creador del formulario de multimedia para proceder ahora a crear el formulario

---
+++ src/AppBundle/Form/Type/MultimediaType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use AppBundle\Entity\Multimedia;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MultimediaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('multimedia', FileType::class ,array('label' => 'Fichero multimedia del articulo'))
+            ->add('armario');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Multimedia::class
+        ]);
+    }
+}

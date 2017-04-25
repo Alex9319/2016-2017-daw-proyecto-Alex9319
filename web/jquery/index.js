5ac8f3cd61bc7f32d9d2a28fc46d038aa3409e39
Alejandro Arevalo <alejandro19193@gmail.com>
2017-04-25 12:14:13 +0200

    Cambiado el Estilo de la aplicación

--- web/jquery/index.js
+++ web/jquery/index.js
@@ -1,9 +1,26 @@
-$("#usuario").mouseenter(function() {
-	$("#desplegable").show('slow');
-});
-								
-$("#desplegable").mouseleave(function() {
-	$("#desplegable").hide('slow');
-});
+$(".desplegable").hide();
+
+if((screen.width>=992) && (screen.height>=768)){
+	$("#usuario").mouseenter(function() {
+		$(".desplegable").show();
+	});
+
+	$("#usuario").mouseleave(function() {
+		$(".desplegable").hide();
+	});
+	
+	$(".desplegable").mouseenter(function() {
+		$(".desplegable").show();
+	});
+
+	$(".desplegable").mouseleave(function() {
+		$(".desplegable").hide();
+	});	
+}
+else{
+	$("#usuario").click(function() {
+		$(".desplegable").toggle();
+	});
+}
 	
 $('select').select2();

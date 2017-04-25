843962a6515f1c976965072377ccff140398d517
Alejandro Arevalo <alejandro19193@gmail.com>
2017-04-25 14:05:49 +0200

    Editado el Listado de archivador y el Controller para que se pueda ordenar por los diferentes campos y que se muestre el numero total de elementos

--- src/AppBundle/Controller/ArchivadorController.php
+++ src/AppBundle/Controller/ArchivadorController.php
@@ -16,19 +16,23 @@ class ArchivadorController extends Controller
     /**
      * @Route("/archivador", name="listadoArchivadores")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         /** @var EntityManager $em */
         $em = $this->getDoctrine()->getManager();
-        $archivadores = $em->createQueryBuilder()
+        $query = $em->createQueryBuilder()
             ->select('a')
             ->from('AppBundle:Archivador', 'a')
             ->getQuery()
             ->getResult();
 
-        return $this->render('archivador/listar.html.twig', [
-            'archivadores' => $archivadores,
-        ]);
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*numero de pagina*/
+        );
+
+        return $this->render('archivador/listar.html.twig', array('pagination' => $pagination));
     }
 
     /**

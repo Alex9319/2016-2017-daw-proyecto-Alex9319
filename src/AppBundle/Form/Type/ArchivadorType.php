05b2d988af9be791e55caa6ce0b80eebb752bb6b
Alejandro Arevalo <alejandro19193@gmail.com>
2017-05-09 17:38:57 +0200

    Añadidos las indicaciones para introducir datos en los formularios y puestos los campos requeridos

--- src/AppBundle/Form/Type/ArchivadorType.php
+++ src/AppBundle/Form/Type/ArchivadorType.php
@@ -12,9 +12,27 @@ class ArchivadorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('numero')
-            ->add('color')
-            ->add('armario');
+            ->add('numero',null,[
+                'label'=> 'Numero del Archivador',
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => 'Introduzca el numero del Archivador'
+                )
+            ])
+            ->add('color',null,[
+                'label'=> 'Color del Archivador',
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => 'Introduzca el color de la etiqueta del Archivador'
+                )
+            ])
+            ->add('armario',null,[
+                'label'=> 'Armario',
+                'required' => false,
+                'attr' => array(
+                    'placeholder' => 'Seleccione el Armario en el que se encuentra'
+                )
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

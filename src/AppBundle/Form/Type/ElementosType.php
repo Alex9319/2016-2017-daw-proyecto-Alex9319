fee901add6ac8e381057a711ca8146fb599afbc8
Alejandro Arevalo <alejandro19193@gmail.com>
2017-05-09 17:33:58 +0200

    Añadidos las indicaciones para introducir datos en los formularios y puestos los campos requeridos

--- src/AppBundle/Form/Type/ElementosType.php
+++ src/AppBundle/Form/Type/ElementosType.php
@@ -4,7 +4,7 @@ namespace AppBundle\Form\Type;
 
 use AppBundle\Entity\Elementos;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,25 +14,49 @@ class ElementosType extends AbstractType
     {
         $builder
             ->add('nombre', null, [
-                'label' => 'Nombre del Articulo'
+                'label' => 'Nombre del Articulo',
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => 'Introduzca el nombre del Articulo'
+                )
             ])
-            ->add('observaciones', null, [
-                'label' => 'Observaciones del Articulo'
+            ->add('observaciones', TextareaType::class, [
+                'label' => 'Observaciones del Articulo',
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => 'Introduzca la descripcion del articulo'
+                )
             ])
             ->add('nivelDeAcceso', null, [
-                'label' => 'Nivel de acceso del Articulo'
+                'label' => 'Nivel de acceso del Articulo',
+                'required' => true,
+                'attr'=>array(
+                    'placeholder' =>'Introduzca el nivel de acceso'
+                )
             ])
             ->add('fechaAlta', null, [
-                'label' => 'Fecha de alta del Articulo'
+                'label' => 'Fecha de Alta',
+                'widget' => 'single_text',
+                'required' => true
             ])
             ->add('archivador', null, [
-                'label' => 'Archivador donde se encuentra el Articulo'
+                'label' => 'Archivador donde se encuentra el Articulo',
+                'attr'=>array(
+                    'placeholder'=>'Seleccione el Archivador'
+                )
             ])
             ->add('armario', null, [
-                'label' => 'Armario donde se encuentra el Articulo'
+                'label' => 'Armario donde se encuentra el Articulo',
+                'attr'=>array(
+                    'placeholder'=>'Selecciona el Armario'
+                )
             ])
             ->add('categoria', null, [
-                'label' => 'Categoria del Articulo'
+                'label' => 'Categoria del Articulo',
+                'required' => true,
+                'attr'=>array(
+                    'placeholder'=>'Seleccione la Categoria'
+                )
             ]);
     }
 

732c960011e8eb1d39dfe929684773cc5101d369
Alejandro Arevalo <alejandro19193@gmail.com>
2017-05-25 18:24:46 +0200

    Añadido el formulario variable el cual falla en la recogida de datos por el ajax

--- src/AppBundle/Form/Type/ElementosType.php
+++ src/AppBundle/Form/Type/ElementosType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form\Type;
 
 use AppBundle\Entity\Elementos;
+use AppBundle\Form\Listener\AddArchivador;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -43,21 +44,27 @@ class ElementosType extends AbstractType
                 'required' => true,
                 'placeholder'=>'dd/mm/aaaa'
             ])
-            ->add('archivador', null, [
-                'label' => 'Archivador donde se encuentra el Articulo',
-                'placeholder'=>'Seleccione el Archivador',
-                'required' => false,
-            ])
             ->add('armario', null, [
                 'label' => 'Armario donde se encuentra el Archivador',
                 'placeholder'=>'Seleccione el un Armario',
-                'required' => false,
+                'class'=>'AppBundle\Entity\Armario',
+                'required' => false
+            ])
+            ->add('archivador', null, [
+                'label' => 'Archivador donde se encuentra el Articulo',
+                'placeholder'=>'Seleccione el Archivador',
+                'class'=>'AppBundle\Entity\Archivador',
+                'required' => false
             ])
             ->add('categoria', null, [
                 'label' => 'Categoria del Articulo',
                 'required' => true,
                 'placeholder'=>'Seleccione la Categoria'
             ]);
+        // Añadimos un EventListener que actualizará el campo archivadores
+        // para que sus opciones correspondan
+        // con el armario seleccionado por el usuario
+        $builder->addEventSubscriber(new AddArchivador());
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -66,5 +73,9 @@ class ElementosType extends AbstractType
             'data_class' => Elementos::class,
             'translation_domain' => false
         ]);
+    }
+    public function getNombre()
+    {
+        return 'nombre';
     }
 }

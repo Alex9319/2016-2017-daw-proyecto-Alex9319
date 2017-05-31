4b57014bfa0303d776ca34c8b3540704f1f60551
Alejandro Arevalo <alejandro19193@gmail.com>
2017-05-31 13:10:29 +0200

    Ajustado el select2 para que cuando cambie el tamaño de la pantalla se ajuste el propio select2 al tamaño y metido todo dentro de la función document ready

--- web/jquery/index.js
+++ web/jquery/index.js
@@ -1,38 +1,41 @@
-$(".desplegable").hide();
+$(document).ready(function() {
+    $(".desplegable").hide();
 
-if((screen.width>=992) && (screen.height>=768)){
-	$("#usuario").mouseenter(function() {
-		$(".desplegable").show();
-	}).mouseleave(function() {
-		$(".desplegable").hide();
-	});
-	
-	$(".desplegable").mouseenter(function() {
-		$(".desplegable").show();
-	}).mouseleave(function() {
-		$(".desplegable").hide();
-	});	
-}
-else{
-	$("#usuario").click(function() {
-		$(".desplegable").toggle();
-	});
-}
-	
-$('select').select2();
+    if ((screen.width >= 992) && (screen.height >= 768)) {
+        $("#usuario").mouseenter(function () {
+            $(".desplegable").show();
+        }).mouseleave(function () {
+            $(".desplegable").hide();
+        });
 
-$('.cerrar').click(function(){
-	fina=$("audio").length;
-	finv=$("video").length;
-    if(fina >= 0 ) {
-		for (var i = 0; i < fina; i++) {
-			$('audio')[i].pause();
-		}
-	}
-	if(finv >= 0 ) {
-		for (var j = 0; j < finv; j++) {
-			$('video')[j].pause();
-		}
-	}
+        $(".desplegable").mouseenter(function () {
+            $(".desplegable").show();
+        }).mouseleave(function () {
+            $(".desplegable").hide();
+        });
+    }
+    else {
+        $("#usuario").click(function () {
+            $(".desplegable").toggle();
+        });
+    }
+    $(window).resize(function () {
+        $('select').select2();
+    });
+    $('select').select2();
+
+    $('.cerrar').click(function () {
+        fina = $("audio").length;
+        finv = $("video").length;
+        if (fina >= 0) {
+            for (var i = 0; i < fina; i++) {
+                $('audio')[i].pause();
+            }
+        }
+        if (finv >= 0) {
+            for (var j = 0; j < finv; j++) {
+                $('video')[j].pause();
+            }
+        }
+    });
 });
-
